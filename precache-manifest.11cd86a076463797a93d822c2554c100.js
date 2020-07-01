dae6ac099f813d7ded7666ec7bbfc1c096e4229b
vivipure <vivipureheart@gmail.com>
2020-07-01 12:40:40 +0000

    Deploying to gh-pages from master 18a2edd01bd63122dbebe5c43f965efcfa796f78

---
+++ precache-manifest.11cd86a076463797a93d822c2554c100.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11d35cae783ca28e4d36d6ffd31f513",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dcb53da84d5d84996ac3",
+    "url": "./static/css/main.215ff7dc.chunk.css"
+  },
+  {
+    "revision": "05f684ff81548b1df3e2",
+    "url": "./static/js/2.36b6a52d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.36b6a52d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcb53da84d5d84996ac3",
+    "url": "./static/js/main.464469d5.chunk.js"
+  },
+  {
+    "revision": "32ac97a9fd00a06e0ce7",
+    "url": "./static/js/runtime-main.b78c3cdf.js"
+  },
+  {
+    "revision": "c4edf1a2f4fe83f3e1b4dbc9180630b1",
+    "url": "./static/media/email.c4edf1a2.svg"
+  },
+  {
+    "revision": "8d26c43e7e772718d75988a9d8102346",
+    "url": "./static/media/phone.8d26c43e.svg"
+  },
+  {
+    "revision": "8336136a1224c5c47746e0a851b4bdcd",
+    "url": "./static/media/wechat.8336136a.jpg"
+  }
+]);
